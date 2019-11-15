5c7b8dd7aed10499e8d7757b0c2b5b044c675fe6
ipercyNC <ipercy@pdx.edu>
2019-11-15 15:45:54 -0800

    CS588 Assignment 3 Part 3
    PySpark code for part 3

---
+++ assignment3_mascotcount.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+"""BigQuery I/O PySpark example."""
+from pyspark.sql import SparkSession
+
+spark = SparkSession \
+          .builder \
+            .master('yarn') \
+              .appName('spark-bigquery-demo') \
+                .getOrCreate()
+
+                # Use the Cloud Storage bucket for temporary BigQuery export data used
+                # by the connector. This assumes the Cloud Storage connector for
+                # Hadoop is configured.
+bucket = spark.sparkContext._jsc.hadoopConfiguration().get(
+                    'fs.gs.system.bucket')
+spark.conf.set('temporaryGcsBucket', bucket)
+
+            # Load mastcot data from BigQuery.
+mascots = spark.read.format('bigquery') \
+    .option('table', 'bigquery-public-data.ncaa_basketball.mascots') \
+    .load()
+mascots.createOrReplaceTempView('mascots')
+
+            # Load color data from BigQuery
+colors = spark.read.format('bigquery') \
+        .option('table','bigquery-public-data.ncaa_basketball.team_colors')\
+        .load()
+
+colors.createOrReplaceTempView('colors') 
+
+                                # Perform mascot + color count.
+mascots_join = spark.sql(
+    'SELECT coalesce(m.tax_genus,m.non_tax_type) as type, c.color, count(c.id) as cnt FROM mascots m, colors c where m.id = c.id group by type, c.color order by cnt desc, type desc')
+mascots_join.show()
+mascots_join.printSchema()
+
+                                    # Saving the data to BigQuery
+mascots_join.write.format('bigquery') \
+        .option('table', 'wordcount_dataset.mascot_team_color_count_output') \
+        .save()
